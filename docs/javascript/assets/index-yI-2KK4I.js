0b91f35cc2ed09333ead40002dbf2d0f659169a9
Sadanand Pai <sadypai@gmail.com>
2023-11-28 18:27:51 +0000

    build files - github action

---
+++ docs/javascript/assets/index-yI-2KK4I.js
@@ -0,0 +1,1 @@
+import"./challenges-JjuJeQgF.js";import"./navbar-W71TuEFY.js";const e=document.querySelector(".burger-menu"),t=document.querySelector(".nav-links");e.addEventListener("click",()=>{t.classList.toggle("active")});
